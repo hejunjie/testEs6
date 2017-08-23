b55da884f66c681280d511121975ed612e0e3a89
hejunjie <798085786@qq.com>
2017-08-23 14:35:45 +0800

    init

---
+++ src/testes6.js
@@ -0,0 +1,38 @@
+
+
+// let a='x';
+//
+// [a,b,c]=[1222,2,3];
+// {
+// let e='xx';
+// }
+// console.log(a);
+// console.log(`asdflsdf line1
+// fslfsldkfj line2`);
+//
+// {
+// let m;
+// let e='sfsf';
+// m= e => e+'aaa';
+// console.log(m(e));
+// alert('fff');
+
+// console.log(/a-z/.test('s'));
+// console.log(Number('0o123'));
+// console.log(Number('0b111'));
+// console.log(Number.EPSILON<0.00000000000001);
+// console.log(Number.MAX_SAFE_INTEGER);
+// console.log(Number.MIN_SAFE_INTEGER);
+// console.log(Number.isSafeInteger(23123123312312312313213));
+// function fasd(a=8) {
+//     "use strict";
+//     console.log(a);
+// }
+//
+// fasd();
+
+function testrest(...s){
+    console.log(s);
+
+}
+testrest('a','b');
